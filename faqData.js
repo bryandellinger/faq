ebca028c15cd0c72793f807826fb1873fcf2a068
bryandellinger <dellingerfam5@gmail.com>
2021-08-01 16:37:11 -0400

    Add files via upload

---
+++ faqData.js
@@ -0,0 +1,110 @@
+var data = {
+    "panels": [
+        {
+            "title": "Academic",
+            "id": "headingOne",
+            "children": [
+                {
+                    "title": "AY20 Academic Calendar",
+                    "aspx": "AY_Calendar"
+                },
+                {
+                    "title": "Compass",
+                    "aspx": "Compass"
+                },
+                {
+                    "title": "Gift Funds",
+                    "aspx": "Gift_Funds"
+                },
+                {
+                    "title": "Manuscript Clearance for Public Release",
+                    "aspx": "Manuscript_Clearance_for_Public_Release"
+                },
+                {
+                    "title": "Mementos",
+                    "aspx": "Mementos"
+                }
+            ]
+        },
+        {
+            "title": "Admin",
+            "id": "headingTwo",
+            "children": [
+                {
+                    "title": "Alternate_Work_Schedule",
+                    "aspx": "Alternate Work Schedule"
+                },
+                {
+                    "title": "Civilian Personnel System Access Requests",
+                    "aspx": "Civilian_Personnel_System_Access_Requests"
+                },
+                {
+                    "title": "Comp Time / Overtime Form",
+                    "aspx": "Comp_Time_Form"
+                },
+                {
+                    "title": "CSL Leadership Contact Numbers",
+                    "aspx": "CSL_Leadership_Contact_Numbers"
+                },
+                {
+                    "title": "Federal and Training Holidays",
+                    "aspx": "Federal_and_Training_Holidays"
+                },
+                {
+                    "title": "Gears",
+                    "aspx": "Gears"
+                },
+                {
+                    "title": "HHD Leave and Passes",
+                    "aspx": "HHD_Leave_and_Passes"
+                },
+                {
+                    "title": "Military Leave Requests",
+                    "aspx": "Military Leave Requests"
+                },
+                {
+                    "title": "NCOER",
+                    "aspx": "NCOER"
+                },
+                {
+                    "title": "Pentagon",
+                    "aspx": "Pentagon"
+                }
+            ]
+        },
+        {
+            "title": "Awards",
+            "id": "headingThree",
+            "children": [
+                {
+                    "title": "Civilian Achievement Awards",
+                    "aspx": "Civilian_Achievement_Awards"
+                },
+                {
+                    "title": "Civilian_of_the_Quarter_Awards",
+                    "aspx": "Civilian of the Quarter Awards"
+                },
+                {
+                    "title": "Comp Time / Overtime Form",
+                    "aspx": "Comp_Time_Form"
+                },
+                {
+                    "title": "Civilian Time Off Awards",
+                    "aspx": "Civilian_Time_Off_Awards"
+                },
+                {
+                    "title": "COMDT Coin Certificate Award",
+                    "aspx": "COMDT_Coin_Certificate_Award"
+                },
+                {
+                    "title": "Military Recommendation for Award",
+                    "aspx": "Military_Recommendation_for_Award"
+                },
+                {
+                    "title": "Systen_for_Award_Management",
+                    "aspx": "Systen_for_Award_Management"
+                }
+            ]
+        }
+    ]
+};
